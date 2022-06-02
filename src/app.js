21906575a00f10f81d5c89148196aeab58ff54f0
juancrcabrera <juancrcabrera@gmail.com>
2022-06-02 10:13:24 -0300

    grammar

--- src/app.js
+++ src/app.js
@@ -1,22 +1,22 @@
-const express = require('express'); //requiero express
-const path = require('path'); //requiero path
+const express = require('express');
+const path = require('path');
 
-//creo objeto app que ejecuta express
+//Creo objeto app que ejecuta express
 let app = express(); 
 
 app.use(express.urlencoded({extended:false}));
 
-//defino que obtendré mis recursos estáticos desde la carpeta public
+//Defino que obtendré mis recursos estáticos desde la carpeta public
 const publicPath = path.join(__dirname,'../public');
 app.use(express.static(publicPath));
 
 const port = process.env.PORT || 3000
-//defino el puerto
+//Defino el puerto
 app.listen(port, ()=>{
     console.log(`Servidor conectado al puerto ${port}`)
 });
 
-//defino ñas rutas vinculandolas con un html 
+//Defino las rutas vinculandolas con un html 
 
 const homePath = path.join(__dirname, "/views/home.html")
 app.get("/",(req, res)=>{
@@ -33,7 +33,7 @@ app.get("/login",(req, res)=>{
 res.sendFile(loginPath);
 })
 
-//las rutas a donde se enviarán los datos
+//Las rutas a donde se enviarán los datos
 
 app.post("/datos-registro",(req, res)=>{
 res.send(req.body);
