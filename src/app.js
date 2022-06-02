75e4c61f1e2c6e7579d6480d8df15b51264d923f
juancrcabrera <juancrcabrera@gmail.com>
2022-06-01 23:28:46 -0300

    added: login and register pages

---
+++ src/app.js
@@ -0,0 +1,44 @@
+const express = require('express'); //requiero express
+const path = require('path'); //requiero path
+
+//creo objeto app que ejecuta express
+let app = express(); 
+
+app.use(express.urlencoded({extended:false}));
+
+//defino que obtendré mis recursos estáticos desde la carpeta public
+const publicPath = path.join(__dirname,'../public');
+app.use(express.static(publicPath));
+
+const port = process.env.PORT || 3000
+//defino el puerto
+app.listen(port, ()=>{
+    console.log(`Escuchando por el puerto ${port}`)
+});
+
+//defino ñas rutas vinculandolas con un html 
+
+const homePath = path.join(__dirname, "/views/home.html")
+app.get("/",(req, res)=>{
+res.sendFile(homePath);
+})
+
+const registerPath = path.join(__dirname, "/views/register.html")
+app.get("/register",(req, res)=>{
+res.sendFile(registerPath);
+})
+
+const loginPath = path.join(__dirname, "/views/login.html")
+app.get("/login",(req, res)=>{
+res.sendFile(loginPath);
+})
+
+//las rutas a donde se enviarán los datos
+
+app.post("/datos-registro",(req, res)=>{
+res.send(req.body);
+})
+
+app.post("/datos-login",(req, res)=>{
+    res.send(req.body);
+})
